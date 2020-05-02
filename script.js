bb046f303d857459b2f55ec40c3a7614ed743196
osamaadelmahdy <osamaadelmahdy@gmail.com>
2020-05-02 07:47:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let x = document.getElementById("open");
+let y = document.getElementById("list");
+$(document).ready(function(){
+    $(".open").click(function(){
+        $(".listinnav").hide();
+    });
+});
